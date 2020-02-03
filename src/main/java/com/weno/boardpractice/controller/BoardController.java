344a5837f3ad2095bee3347ebe251385812135fa
logicstark <logicalstark1105@gmail.com>
2020-02-03 18:36:04 +0900

    Board Done

---
+++ src/main/java/com/weno/boardpractice/controller/BoardController.java
@@ -0,0 +1,77 @@
+package com.weno.boardpractice.controller;
+
+
+import com.weno.boardpractice.dto.BoardDto;
+import com.weno.boardpractice.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class BoardController {
+
+    private BoardService boardService;
+
+    @GetMapping("/")
+    public String list(Model model){
+        List<BoardDto> boardDtoList = boardService.getBoardlist();
+        model.addAttribute("boardList", boardDtoList);
+        return "/board/list.html";
+    }
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto = boardService.getPost(no);
+        model.addAttribute("boardDto", boardDto);
+        return "/board/detail.html";
+    }
+
+    @GetMapping("/post/edit{no}")
+    public String edit(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "/board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
+
+        return "redirect:/";
+    }
+
+
+
+
+
+
+
+
+
+    @GetMapping("/post")
+    public String write(){
+        return "/board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto){
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+
+
+}
---
+++ src/main/java/com/weno/boardpractice/service/BoardService.java
@@ -0,0 +1,67 @@
+package com.weno.boardpractice.service;
+
+import com.weno.boardpractice.domain.entity.BoardEntity;
+import com.weno.boardpractice.domain.repository.BoardRepository;
+import com.weno.boardpractice.dto.BoardDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class BoardService {
+
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public Long savePost(BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardlist() {
+
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for(BoardEntity boardEntity : boardEntityList){
+            BoardDto boardDto = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDto);
+        }
+
+        return boardDtoList;
+
+    }
+
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDTO = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDTO;
+
+    }
+
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
+    }
+}
