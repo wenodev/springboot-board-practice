c2f7bad3cc63979f124b48b61f718a007d88212b
logicstark <logicalstark1105@gmail.com>
2020-02-13 11:35:06 +0900

    login.html signup.html design done

--- src/main/java/com/weno/boardpractice/BoardPracticeApplication.java
+++ src/main/java/com/weno/boardpractice/BoardPracticeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BoardPracticeApplication {
-// a
+
 	public static void main(String[] args) {
 		SpringApplication.run(BoardPracticeApplication.class, args);
 	}
---
+++ src/main/java/com/weno/boardpractice/domain/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.weno.boardpractice.domain.repository;
+
+import com.weno.boardpractice.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity,Long> {
+
+
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
